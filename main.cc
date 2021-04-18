f9a43b55c325abb9af25c5400a332c86b829f285
Blakeb57 <bb148917@ohio.edu>
2021-04-18 15:03:38 -0400

    going

--- main.cc
+++ main.cc
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <list>
+#include <string>
+#include <fstream>
+#include "closet_item.h"
+using namespace std;
 
+int closet();
+
+int main()
+{
+    int choice;
+    ifstream ifs;
+    string item;
+
+    list<closet*> my_closet;
+    list<closet*>::iterator c;
+    closet_item *tmp;
+
+    ifs.open()
+}
